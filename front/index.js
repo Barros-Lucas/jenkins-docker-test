559af924a411120547189b22eaaa7d7d0d1280f0
lucas <lucas.barros@hotmail.fr>
2020-03-27 17:04:18 +0100

    Feat: new front antoine

--- front/index.js
+++ front/index.js
@@ -1,7 +1,20 @@
 function sendUserName(){
 	var userName = $("#userName").val();
-	var url = "http://90.89.7.235:8081/getViewerCount?boxname=nbviewers1&username="+userName
-	$.get(url, function( data ) {
-	  $("#nbviewers1").text(data);
-	});
+var url = "http://90.89.7.235:8082/getViewerCount?boxname=nbviewers1&username="+userName
+				$.get(url, function( data ) {
+				  $("#nbviewers1").text(data);
+				});
+var url = "http://90.89.7.235:8082/getProfilePict?boxname=image1&username="+userName
+				$.get(url, function( data ) {
+				  $("#image1").attr("src", data);
+				});
+}
+
+function fillCompareColumn(index) {
+	var userCompare = $("#userCompare").val();
+	$("#user" + index).text(userCompare);
+var url = "http://90.89.7.235:8082/getViewerCount?boxname=table&username="+userCompare
+					$.get(url, function( data ) {
+					  $("#getViewerCount + index").text(data);
+					});
 }
