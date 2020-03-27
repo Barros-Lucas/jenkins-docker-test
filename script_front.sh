25a2ad4731221d05050a9f93d931d1c3718dff43
Barros-Lucas <38254963+Barros-Lucas@users.noreply.github.com>
2020-03-27 14:03:55 +0100

    replace back with front

--- script_front.sh
+++ script_front.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker ps -q --filter ancestor=dashboard/back-test-jenkins | grep -q . && docker kill $(docker ps -q --filter ancestor=dashboard/back-test-jenkins)
+docker ps -q --filter ancestor=dashboard/front-test-jenkins | grep -q . && docker kill $(docker ps -q --filter ancestor=dashboard/front-test-jenkins)
 
-docker run -d -p 8082:80 dashboard/back-test-jenkins
+docker run -d -p 8082:80 dashboard/front-test-jenkins
