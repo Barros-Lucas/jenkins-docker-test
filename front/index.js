e1d62ed9eb8dcb20509434a6e98f63416b784471
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2020-03-27 17:23:50 +0100

    send Files

--- front/index.js
+++ front/index.js
@@ -1,20 +1,16 @@
 function sendUserName(){
 	var userName = $("#userName").val();
-var url = "http://90.89.7.235:8082/getViewerCount?boxname=nbviewers1&username="+userName
+var url = "http://90.89.7.235:8082/nbViewers?boxname=textBox&username="+userName
 				$.get(url, function( data ) {
-				  $("#nbviewers1").text(data);
+				  $("#textBox").text(data);
 				});
-var url = "http://90.89.7.235:8082/getProfilePict?boxname=image1&username="+userName
+var url = "http://90.89.7.235:8082/nbViewers?boxname=textBox2&username="+userName
 				$.get(url, function( data ) {
-				  $("#image1").attr("src", data);
+				  $("#textBox2").text(data);
 				});
 }
 
 function fillCompareColumn(index) {
 	var userCompare = $("#userCompare").val();
 	$("#user" + index).text(userCompare);
-var url = "http://90.89.7.235:8082/getViewerCount?boxname=table&username="+userCompare
-					$.get(url, function( data ) {
-					  $("#getViewerCount + index").text(data);
-					});
 }
