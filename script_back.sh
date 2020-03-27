347e0777617b589afac536565e7576bf6d147f14
lucas <lucas.barros@hotmail.fr>
2020-03-27 17:03:43 +0100

    Feat: silience for back docker run

--- script_back.sh
+++ script_back.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker ps -q --filter ancestor=dashboard/back-test-jenkins | grep -q . && docker kill $(docker ps -q --filter ancestor=dashboard/back-test-jenkins)
 
-docker run -p 8082:8082 dashboard/back-test-jenkins
+docker run -d --log-driver=none -p 8082:8082 dashboard/back-test-jenkins
