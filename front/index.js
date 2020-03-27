f620a72d2dbe26d06812417afd72034c667cc0fc
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2020-03-27 17:29:55 +0100

    send new file

--- front/index.js
+++ front/index.js
@@ -1,16 +1,32 @@
 function sendUserName(){
 	var userName = $("#userName").val();
-var url = "http://90.89.7.235:8082/nbViewers?boxname=textBox&username="+userName
+var url = "http://90.89.7.235:8082/getViewerCount?boxname=nbviewers1&username="+userName
 				$.get(url, function( data ) {
-				  $("#textBox").text(data);
+				  $("#nbviewers1").text(data);
 				});
-var url = "http://90.89.7.235:8082/nbViewers?boxname=textBox2&username="+userName
+var url = "http://90.89.7.235:8082/getProfilePict?boxname=image1&username="+userName
 				$.get(url, function( data ) {
-				  $("#textBox2").text(data);
+				  $("#image1").attr("src", data);
+				});
+var url = "http://90.89.7.235:8082/getViewerCount?boxname=nbviewers1&username="+userName
+				$.get(url, function( data ) {
+				  $("#nbviewers1").text(data);
+				});
+var url = "http://90.89.7.235:8082/getProfilePict?boxname=image1&username="+userName
+				$.get(url, function( data ) {
+				  $("#image1").attr("src", data);
 				});
 }
 
 function fillCompareColumn(index) {
 	var userCompare = $("#userCompare").val();
 	$("#user" + index).text(userCompare);
+var url = "http://90.89.7.235:8082/getViewerCount?boxname=table&username="+userCompare
+					$.get(url, function( data ) {
+					  $("#getViewerCount + index").text(data);
+					});
+var url = "http://90.89.7.235:8082/getViewerCount?boxname=table&username="+userCompare
+					$.get(url, function( data ) {
+					  $("#getViewerCount + index").text(data);
+					});
 }
