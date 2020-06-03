340f72f0162698fd0225ae72093afe6b087ac466
Barros-Lucas <38254963+Barros-Lucas@users.noreply.github.com>
2020-06-03 14:08:09 +0200

    Update script_front.sh

--- script_front.sh
+++ script_front.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker ps -q --filter publish=8081 | grep -q . && docker kill $(docker ps -q --filter publish=8081)
+docker ps -q --filter publish=80 | grep -q . && docker kill $(docker ps -q --filter publish=80)
 
 docker run -d -p 8081:80 dashboard/front-test-jenkins
