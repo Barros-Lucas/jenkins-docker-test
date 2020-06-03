c7748c6179e27c7d43cc5d0a8f438f4367a94cd7
Barros-Lucas <38254963+Barros-Lucas@users.noreply.github.com>
2020-06-03 12:27:04 +0200

    Update script_back.sh

--- script_back.sh
+++ script_back.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker ps -q --filter  publish=8081 | grep -q . && docker kill $(docker ps -q --filter  publish=8081 )
+docker ps -q --filter  publish=8082 | grep -q . && docker kill $(docker ps -q --filter  publish=8082)
 
 docker run -d --log-driver=none -p 8082:8082 dashboard/back-test-jenkins
