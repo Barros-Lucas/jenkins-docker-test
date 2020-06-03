a099e48c2396aefb071d5ae6c01c1c4fd10545e0
JRochelli <jonathan.rochelli@yahoo.com>
2020-06-03 14:26:52 +0200

    Test autopush

--- front/index.js
+++ front/index.js
@@ -4,14 +4,6 @@ var url = "http://90.89.7.235:8082/getViewerCount?boxname=nbviewers1&username="+
 				$.get(url, function( data ) {
 				  $("#nbviewers1").text(data);
 				});
-var url = "http://90.89.7.235:8082/getProfilePict?boxname=image1&username="+userName
-				$.get(url, function( data ) {
-				  $("#image1").attr("src", data);
-				});
-var url = "http://90.89.7.235:8082/getProfilePict?boxname=image2&username="+userName
-				$.get(url, function( data ) {
-				  $("#image2").attr("src", data);
-				});
 }
 
 function fillCompareColumn(index) {
