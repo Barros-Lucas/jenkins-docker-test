b30419b5bb4417fe613fea2366f8cc64a7decd45
Barros-Lucas <38254963+Barros-Lucas@users.noreply.github.com>
2020-06-03 12:13:55 +0200

    change filter of ps docker fo kill by port
    bug if we kill from name so i decided to kill by port

--- script_front.sh
+++ script_front.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker ps -q --filter ancestor=dashboard/front-test-jenkins | grep -q . && docker kill $(docker ps -q --filter ancestor=dashboard/front-test-jenkins)
+docker ps -q --filter publish=8081 | grep -q . && docker kill $(docker ps -q --filter publish=8081)
 
 docker run -d -p 8081:80 dashboard/front-test-jenkins
